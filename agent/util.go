e96ea980f6844c9e9c494d3857ace43936054f61
Atish Nazir <github@cognitive-i.com>
2020-07-31 15:13:03 +0100

    Utils: update encode/decode to handle plus escaping
    Plus/Percent encoding is used in the KDF field

--- agent/util.go
+++ agent/util.go
@@ -1,16 +1,45 @@
 package agent
 
-import "strings"
+import (
+	"bytes"
+	"strconv"
+	"strings"
+)
 
 var (
-	decoder = strings.NewReplacer("%25", "%", "%0D", "\r", "%0A", "\n")
-	encoder = strings.NewReplacer("%", "%25", "\r", "%0D", "\n", "%0A")
+	decoderPlus = strings.NewReplacer("+", " ")
+	encoderPlus = strings.NewReplacer(" ", "+", "+", "%2B")
+	encoder     = strings.NewReplacer("%", "%25", "\r", "%0D", "\n", "%0A")
 )
 
 func decode(source string) string {
-	return decoder.Replace(source)
+	result := bytes.NewBuffer(nil)
+	length := len(source)
+
+	for i := 0; i < length; {
+		b := source[i]
+		if b == '%' && (2 <= (length - i)) {
+			v, _ := strconv.ParseUint(source[i+1:i+3], 16, 8)
+			b = byte(v)
+			i += 2
+		}
+
+		result.WriteByte(b)
+
+		i++
+	}
+
+	return result.String()
 }
 
 func encode(source string) string {
 	return encoder.Replace(source)
+}
+
+func decodeWithPlus(source string) string {
+	return decode(decoderPlus.Replace(source))
+}
+
+func encodeWithPlus(source string) string {
+	return encoderPlus.Replace(encode(source))
 }
--- agent/util_test.go
+++ agent/util_test.go
@@ -20,6 +20,22 @@ func TestUtil_DecodeUsualEscapees(t *testing.T) {
 	Expect(decode(input)).To(Equal(output))
 }
 
+func TestUtil_DecodeDataEscapes(t *testing.T) {
+	RegisterTestingT(t)
+
+	input := "%31%32%33%34"
+	output := "1234"
+	Expect(decode(input)).To(Equal(output))
+}
+
+func TestUtil_DecodeSpaces(t *testing.T) {
+	RegisterTestingT(t)
+
+	input := "[+%2b+%2b+]"
+	output := "[ + + ]"
+	Expect(decodeWithPlus(input)).To(Equal(output))
+}
+
 func TestUtil_EncodePlainText(t *testing.T) {
 	RegisterTestingT(t)
 
@@ -33,4 +49,12 @@ func TestUtil_EncodeUsualEscapees(t *testing.T) {
 	input := "this shouldn't%be\runchanged\n"
 	output := "this shouldn't%25be%0Dunchanged%0A"
 	Expect(encode(input)).To(Equal(output))
+}
+
+func TestUtil_EncodeDataEscapes(t *testing.T) {
+	RegisterTestingT(t)
+
+	input := "[ + 1 ]"
+	output := "[+%2B+1+]"
+	Expect(encodeWithPlus(input)).To(Equal(output))
 }
